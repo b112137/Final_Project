f8b58be9ea3e0a62585015eb51f64f3b1ec30aa1
b112137 <e0010209@gmail.com>
2021-04-14 16:15:00 +0800

    Chat DB

--- main_project/login/models.py
+++ main_project/login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Chat(models.Model):
+    room = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+    content = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
--- main_project/login/views.py
+++ main_project/login/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse, JsonResponse
 from datetime import datetime
 from django.views.generic import View
 import json
-
+from login.models import Chat
+from rest_framework import serializers
+from django.core import serializers as core_serializers
 
 # Create your views here.
 def login_page(request):
@@ -16,15 +18,22 @@ def chat_update(request):
     if request.method == 'POST':
         post_type = request.POST.get('post_type')
         if post_type == "Send":
-            text = request.POST.get('text')
+            room = request.POST.get('room')
+            user = request.POST.get('user')
+            content = request.POST.get('content')
+            print(content)
             with open('msg.txt', 'a') as msg_file:
-                msg_file.write( text+'\n' )
+                msg_file.write( content+'\n' )
                 msg_file.close()
+
+            Chat.objects.create(room=room, user=user,  content=content)
             return JsonResponse({
                 'result' : "success"
             })
 
         elif post_type == "Receive":
+            room = request.POST.get('room')
+
             history = []
             with open('msg.txt', 'r') as msg_file:
                 line = msg_file.readline()
@@ -33,8 +42,13 @@ def chat_update(request):
                     line = msg_file.readline()
                 msg_file.close()
 
+            # room_history = core_serializers.serialize("json", Chat.objects.all())
+            room_history = core_serializers.serialize("json", Chat.objects.filter(room=room).order_by("time"))
+            # print(room_history)
+
             return JsonResponse({
                 'result' : "success",
-                'msg' : history
+                'msg' : history,
+                'room_history' : room_history,
             })
 
